e2cf50aefebda0a44c0b6ac09b8440e884006b1e
mike030188 <halol.imnida2023@gmail.com>
2024-09-22 23:55:43 +0900

    fix: modify server.js and package.json

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 console.log("Web Server started!");
 const express = require("express");
 const app = express();
-const http = require("http"); // core package
+const PORT = 3000;
+const cors = require("cors");
 
 // 1: Entry code
 app.use(express.json()); // json -> object
+app.use(cors({ origin: "http://localhost:3001" }));
 
 // 2: Session code
 
 // 3: Views code (if we use ejs)
 
 // 4: Router code
-app.get("/", function (req, res) {
-  res.end("Hello World!");
+app.post("/api/message", function (req, res) {
+  const { message } = req.body;
+  if (message) {
+    const responseMessage = message + " + Hello World!";
+    res.json({ response: responseMessage });
+  } else {
+    res.status(400).json({ error: "Message not received" });
+  }
 });
 
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
+app.listen(PORT, function () {
   console.log(`The server is running successfully on port: ${PORT}`);
 });
